5c93d907dc1829b3f6633f0dfd4ee6a7d3e2878c
azeteki <azeteki@safe-mail.net>
2014-07-25 15:00:17 +0100

    (peer view) Code cleanup

--- peers.py
+++ peers.py
@@ -6,30 +6,19 @@ import global_mod as g
 def draw_window(state, window):
     window.clear()
     window.refresh()
+
+    color = curses.color_pair(1)
+    if 'testnet' in state:
+        if state['testnet']: color = curses.color_pair(2)
+
     win_header = curses.newwin(4, 75, 0, 0)
 
-    if 'peerinfo' in state:
-        color = curses.color_pair(1)
-        if 'testnet' in state:
-            if state['testnet']: color = curses.color_pair(2)
-        win_header.addstr(0, 1, "bitcoind-ncurses " + g.version + "     [peer view]      (press 'P' to refresh)", color + curses.A_BOLD)
-
-        window_height = state['y'] - 3
-
-        if len(state['peerinfo']) > window_height:
-            win_header.addstr(1, 1, "connected peers: " + str(len(state['peerinfo'])).ljust(10) + "                        (UP/DOWN: scroll)", curses.A_BOLD)
-        else:
-            win_header.addstr(1, 1, "connected peers: " + str(len(state['peerinfo'])), curses.A_BOLD)
-
-        win_header.addstr(3, 1, "  Node IP                      Version                Recv      Sent", curses.A_BOLD + curses.color_pair(5))
-
-        draw_peers(state)
-
-    else:
-        win_header.addstr(0, 1, "peer info not loaded (this should not happen!)", curses.A_BOLD)
-        win_header.addstr(1, 1, "press 'M' to return to monitor window", curses.A_BOLD)
-
+    win_header.addstr(0, 1, "bitcoind-ncurses " + g.version + "     [peer view]      (press 'P' to refresh)", color + curses.A_BOLD)
+    win_header.addstr(1, 1, "connected peers: " + str(len(state['peerinfo'])).ljust(10) + "                        (UP/DOWN: scroll)", curses.A_BOLD)
+    win_header.addstr(3, 1, "  Node IP                      Version                Recv      Sent", curses.A_BOLD + curses.color_pair(5))
     win_header.refresh()
+
+    draw_peers(state)
 
 def draw_peers(state):
     window_height = state['y'] - 4
@@ -40,16 +29,23 @@ def draw_peers(state):
     for index in xrange(offset, offset+window_height):
         if index < len(state['peerinfo']):
             peer = state['peerinfo'][index]
-            if (index == offset+window_height-1) and (index+1 < len(state['peerinfo'])):
-                win_peers.addstr(index-offset, 3, "... " + peer['addr'])
-            elif (index == offset) and (index > 0):
-                win_peers.addstr(index-offset, 3, "... " + peer['addr'])
+
+            condition = (index == offset+window_height-1) and (index+1 < len(state['peerinfo']))
+            condition = condition or ( (index == offset) and (index > 0) )
+
+            if condition:
+                # scrolling up or down is possible
+                win_peers.addstr(index-offset, 3, "...")
+
             else:
                 if peer['inbound']:
                     win_peers.addstr(index-offset, 1, 'I')
+
                 elif 'syncnode' in peer:
-                    if peer['syncnode']: # syncnodes are outgoing only
+                    if peer['syncnode']:
+                        # syncnodes are outgoing only
                         win_peers.addstr(index-offset, 1, 'S')
+
                 win_peers.addstr(index-offset, 3, peer['addr'])
                 win_peers.addstr(index-offset, 32, peer['subver'].strip("/"))
 
