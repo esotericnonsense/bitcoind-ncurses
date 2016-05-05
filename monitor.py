7ff7cb559e933263468c8ce1689c061093f8d07b
Daniel Edgecumbe <daniele@esotericnonsense.com>
2016-05-05 04:06:40 +0100

    Fix incorrect formatting for hashrate based on diff

--- monitor.py
+++ monitor.py
@@ -128,13 +128,13 @@ def draw_window(state, old_window):
             window.addstr(index, 1, "Est (" + str(block_avg).rjust(4) + "): ~" + "{:,}".format(nextdiff))
 
         if rate > 10**18:
-            rate /= 10**18
+            rate = float(rate)/10**18
             suffix = " EH/s"
         elif rate > 10**12:
-            rate /= 10**12
+            rate = float(rate)/10**12
             suffix = " TH/s"
         else:
-            rate /= 10**6
+            rate = float(rate)/10**6
             suffix = " MH/s"
         rate_string = "{:,}".format(rate) + suffix
         window.addstr(index, 37, "Hashrate (" + str(block_avg).rjust(4) + "): " + rate_string.rjust(13))
